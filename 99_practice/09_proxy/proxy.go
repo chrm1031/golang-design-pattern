b0836492ee4de1f4fe0893c1717bd0f2867617e8
h.miyazaki <umorih1031@gmail.com>
2022-12-28 18:07:39 +0900

    Proxyパターンの練習

---
+++ 99_practice/09_proxy/proxy.go
@@ -0,0 +1,95 @@
+package p_proxy
+
+import "fmt"
+
+/*
+概念的な例
+Nginx のようなウェブ・サーバーは、 アプリケーション・サーバーのプロキシーの役を果たせます
+
+- アプリケーション・サーバーに対するアクセス制御を行う。
+- レート制限を行える。
+- リクエストのキャッシングを行える。
+*/
+
+/*
+server.go: サーバー
+*/
+type server interface {
+	handleRequest(string, string) (int, string)
+}
+
+/*
+nginx.go: プロキシー
+*/
+type Nginx struct {
+	application       *Application
+	maxAllowedRequest int
+	rateLimiter       map[string]int
+}
+
+func newNginxServer() *Nginx {
+	return &Nginx{
+		application:       &Application{},
+		maxAllowedRequest: 2,
+		rateLimiter:       make(map[string]int),
+	}
+}
+
+func (n *Nginx) handleRequest(url, method string) (int, string) {
+	allowed := n.checkRateLimiting(url)
+	if !allowed {
+		return 403, "Not Allowed"
+	}
+	return n.application.handleRequest(url, method)
+}
+
+func (n *Nginx) checkRateLimiting(url string) bool {
+	if n.rateLimiter[url] == 0 {
+		n.rateLimiter[url] = 1
+	}
+	if n.rateLimiter[url] > n.maxAllowedRequest {
+		return false
+	}
+	n.rateLimiter[url] += 1
+	return true
+}
+
+/*
+application.go: 実際のサーバー
+*/
+type Application struct{}
+
+func (a *Application) handleRequest(url, method string) (int, string) {
+	if url == "/app/status" && method == "GET" {
+		return 200, "OK"
+	}
+	if url == "/create/user" && method == "POST" {
+		return 201, "User Created"
+	}
+	return 404, "Not OK"
+}
+
+/*
+main.go: クライアント・コード
+*/
+func main() {
+
+	nginxServer := newNginxServer()
+	appStatusURL := "/app/status"
+	createuserURL := "/create/user"
+
+	httpCode, body := nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createuserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+}
